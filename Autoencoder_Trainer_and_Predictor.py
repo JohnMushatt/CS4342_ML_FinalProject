c4c4ff313f597863332119e1dda862991a823dcb
JohnMushatt <john.mushatt@gmail.com>
2019-12-15 21:26:30 -0500

    Added final model and visuals

--- Autoencoder_Trainer_and_Predictor.py
+++ Autoencoder_Trainer_and_Predictor.py
@@ -1,3 +1,5 @@
+import csv
+
 import pandas as pd
 import numpy as np
 import pickle
@@ -19,8 +21,6 @@ from sklearn.metrics import (confusion_matrix, precision_recall_curve, auc,
                              precision_recall_fscore_support)
 import os
 
-from webencodings import LABELS
-
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # importing or loading the dataset
 training_features = pd.read_csv('trainFt.csv')
@@ -28,19 +28,17 @@ training_lables = pd.read_csv('trainLb.csv')
 testing_data = pd.read_csv('testFt.csv')
 
 # Data with labels attached
-training_features_and_labels = training_features.join(training_lables)
+training_data = training_features.join(training_lables)
 # Split the data into 80% training 20% validation
-training_set, validation_set = train_test_split(training_features_and_labels, test_size=.2)
-# Get only the non-defaulting credit card data
-training_set_with_no_defaulting = training_set[training_set.Class==0]
-#validation_set_with_no_defaulting = validation_set[validation_set.Class==0]
+training_set, validation_set = train_test_split(training_data, test_size=.2)
 
-testing_set = testing_data
+testing_set_transformed = testing_data
 # print(testing_set.shape)
-
+training_set = training_set[training_set.Class==0]
+#validation_set = validation_set[validation_set.Class==0]
 # Get just the features of the training set
-training_set_features_no_defaulting = training_set_with_no_defaulting.iloc[:, :-1]
-training_set_labels_no_defaulting = training_set_with_no_defaulting.iloc[:, -1]
+training_set_features = training_set.iloc[:, :-1]
+training_set_labels = training_set.iloc[:, -1]
 # print(training_set_features.shape)
 
 # Validation data
@@ -51,28 +49,30 @@ validation_set_labels = validation_set.iloc[:, -1]
 sc = StandardScaler()
 
 # Fit on training set only.
-sc.fit(training_set_features_no_defaulting)
+sc.fit(training_set_features)
 
 # Apply transform to both the training set and the test set.
-training_set_transformed_no_defaulting = sc.transform(training_set_features_no_defaulting)
+training_set_transformed = sc.transform(training_set_features)
 validation_set_transformed = sc.transform(validation_set_features)
-testing_set = sc.transform(testing_set)
+testing_set_transformed = sc.transform(testing_set_transformed)
 
 # Get PCA data
 pca = PCA(.95)
-pca.fit(training_set_transformed_no_defaulting)
+pca.fit(training_set_transformed)
 
 # Apply the PCA result to the training and validation
-training_set_PCA_no_defaulting = pca.transform(training_set_transformed_no_defaulting)
-validation_set_PCA = pca.transform(validation_set_transformed)
-project_data_PCA_no_defaulting = pca.transform(testing_set)
+X_train = pca.transform(training_set_transformed)
+X_test = pca.transform(validation_set_transformed)
+principal_testing_data = pca.transform(testing_set_transformed)
 
-print(training_set_PCA_no_defaulting.shape)
-print(validation_set_PCA.shape)
-print(project_data_PCA_no_defaulting.shape)
+#X_train = training_features
+#X_test = validation_set_features
+print(X_train.shape)
+print(X_test.shape)
+print(principal_testing_data.shape)
 
 # Build the autoencoder
-input_dim = training_set_features_no_defaulting.shape[1]
+input_dim = X_train.shape[1]
 print(input_dim)
 
 encoding_dim = 14
@@ -96,24 +96,24 @@ tensorboard = TensorBoard(log_dir='/media/old-tf-hackers-7/logs',
                           write_graph=True,
                           write_images=True)
 # Print some of the stats
-#print(training_set_features.shape)
-#print(training_set_labels.shape)
-#print(validation_set_features.shape)
-#print(validation_set_labels.shape)
+print(training_set_features.shape)
+print(training_set_labels.shape)
+print(validation_set_features.shape)
+print(validation_set_labels.shape)
 autoencoder.summary()
 
-history = autoencoder.fit(training_features, training_features,
+history = autoencoder.fit(X_train, X_train,
                           epochs=nb_epoch,
                           batch_size=batch_size,
                           shuffle=True,
-                          validation_data=(validation_set_features, validation_set_features),
+                          validation_data=(X_test, X_test),
                           verbose=1,
                           ).history
+print("Saving model")
 autoencoder.save('model.h5', overwrite=True)
-
-#autoencoder = load_model('model.h5')
-
-# autoencoder.summary()
+print("Loading model with acc .8386")
+autoencoder = load_model('acc_8386.h5')
+autoencoder.summary()
 loss = history['loss']
 val_loss = history['val_loss']
 epochs = range(nb_epoch)
@@ -122,27 +122,28 @@ plt.plot(epochs, loss, 'bo', label='Training loss')
 plt.plot(epochs, val_loss, 'b', label='Validation loss')
 plt.title('Training and validation loss')
 plt.legend()
-plt.show()
-
-autoencoder = load_model('best_model.h5')
+#plt.show()
+plt.savefig('visuals/tr_val_loss.png', bbox_inches='tight')
 # Predict it
-predictions = autoencoder.predict(validation_set_features)
-mse = np.mean(np.power(validation_set_features - predictions, 2), axis=1)
+pred = autoencoder.predict(X_test)
+mse = np.mean(np.power(X_test - pred, 2), axis=1)
 error_df = pd.DataFrame({'reconstruction_error': mse,
                          'true_class': validation_set_labels})
 print(error_df.describe())
-
 fig = plt.figure()
 ax = fig.add_subplot(111)
 normal_error_df = error_df[(error_df['true_class'] == 0) & (error_df['reconstruction_error'] < 10)]
 _ = ax.hist(normal_error_df.reconstruction_error.values, bins=10)
 
-plt.show()
+#plt.show()
+plt.savefig('visuals/normal_error_defaulting.png', bbox_inches='tight')
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
 fraud_error_df = error_df[error_df['true_class'] == 1]
 _ = ax.hist(fraud_error_df.reconstruction_error.values, bins=10)
-plt.show()
+#plt.show()
+plt.savefig('visuals/fraud_error.png', bbox_inches='tight')
 fpr, tpr, thresholds = roc_curve(error_df.true_class, error_df.reconstruction_error)
 roc_auc = auc(fpr, tpr)
 
@@ -154,27 +155,29 @@ plt.xlim([-0.001, 1])
 plt.ylim([0, 1.001])
 plt.ylabel('True Positive Rate')
 plt.xlabel('False Positive Rate')
-plt.show();
+#plt.show()
+plt.savefig('visuals/ROC.png', bbox_inches='tight')
 
 precision, recall, th = precision_recall_curve(error_df.true_class, error_df.reconstruction_error)
 plt.plot(recall, precision, 'b', label='Precision-Recall curve')
 plt.title('Recall vs Precision')
 plt.xlabel('Recall')
 plt.ylabel('Precision')
-plt.show()
+#plt.show()
+plt.savefig('visuals/Recall_v_precision.png', bbox_inches='tight')
 
 plt.plot(th, precision[1:], 'b', label='Threshold-Precision curve')
 plt.title('Precision for different threshold values')
 plt.xlabel('Threshold')
 plt.ylabel('Precision')
-plt.show()
-
+#plt.show()
+plt.savefig('visuals/precision_threshold.png', bbox_inches='tight')
 plt.plot(th, recall[1:], 'b', label='Threshold-Recall curve')
 plt.title('Recall for different threshold values')
 plt.xlabel('Reconstruction error')
 plt.ylabel('Recall')
-plt.show()
-
+#plt.show()
+plt.savefig('visuals/recall_threshold.png', bbox_inches='tight')
 threshold = 2.9
 
 groups = error_df.groupby('true_class')
@@ -188,13 +191,16 @@ ax.legend()
 plt.title("Reconstruction error for different classes")
 plt.ylabel("Reconstruction error")
 plt.xlabel("Data point index")
-plt.show();
+#plt.show()
+plt.savefig('visuals/reconstruction_errror.png', bbox_inches='tight')
 
-threshold = 5
+threshold = 1.33
 
+
+#--------
+#Predict the training testing data
 y_pred = [1 if e > threshold else 0 for e in error_df.reconstruction_error.values]
 
-# prediciton_labels = validation_set_labels.iloc[:,-1]
 conf_matrix = confusion_matrix(error_df.true_class, y_pred)
 
 plt.figure(figsize=(12, 12))
@@ -203,4 +209,37 @@ sns.heatmap(conf_matrix, xticklabels=['Normal', 'Defaulting'], yticklabels=['Nor
 plt.title("Confusion matrix")
 plt.ylabel('True class')
 plt.xlabel('Predicted class')
-plt.show()
+#plt.show()
+plt.savefig("visuals/confusion_matrix.png", bbox_inches='tight')
+
+
+#Predict final output for project
+final_test_predictions = autoencoder.predict(principal_testing_data)
+final_mse = np.mean(np.power(principal_testing_data - final_test_predictions, 2), axis=1)
+final_error_df = pd.DataFrame({'reconstruction_error': final_mse})
+print(final_error_df.describe())
+final_y_pred = [1 if e > threshold else 0 for e in final_error_df.reconstruction_error.values]
+
+final_default_count =0
+final_nondefault_count = 0
+for e in final_y_pred:
+    if(e==1):
+        final_default_count = final_default_count+1
+    else:
+        final_nondefault_count = final_nondefault_count+1
+training_default_count =0
+training_nondefault_count = 0
+for e in y_pred:
+    if(e==1):
+        training_default_count = training_default_count+1
+    else:
+        training_nondefault_count = training_nondefault_count+1
+
+print("Default count: " + str(final_default_count) + " Non defaulting: " + str(final_nondefault_count))
+print("Default percent of total final test sample " + str(final_default_count/(final_nondefault_count+final_default_count)))
+print("default percent of total training sample " + str(training_default_count / (training_default_count + training_nondefault_count)))
+
+with open('data/final_predictions.csv','w') as result_file:
+    wr = csv.writer(result_file,dialect='excel')
+    wr.writerow(final_y_pred)
+result_file.close()
--- Default_Detection.py
+++ Default_Detection.py
@@ -8,7 +8,8 @@ import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import confusion_matrix, classification_report, auc, precision_recall_curve, roc_curve
+from sklearn.metrics import confusion_matrix, classification_report, auc, precision_recall_curve, roc_curve, \
+    plot_confusion_matrix
 from sklearn.preprocessing import StandardScaler
 from sklearn.manifold import TSNE
 from sklearn.utils import shuffle
@@ -38,6 +39,63 @@ X_validation = remainder.loc[~remainder.index.isin(X_test.index)]
 
 # Reshuffle stuff to make sure random
 
+
+# Shuffle the datasets once more to ensure random feeding into the classification algorithms
 X_train = shuffle(X_train)
 X_test = shuffle(X_test)
-X_validation
+X_validation = shuffle(X_validation)
+X_train_ = shuffle(X_train)
+X_test_ = shuffle(X_test)
+X_validation_ = shuffle(X_validation)
+data_resampled = pd.concat([X_train_, X_test_, X_validation_])
+
+# Normalize the data to an average of 0 and std of 1, but do not touch the Class column
+for feature in X_train.columns.values[:-1]:
+    mean, std = data[feature].mean(), data[feature].std()
+    X_train.loc[:, feature] = (X_train[feature] - mean) / std
+    X_test.loc[:, feature] = (X_test[feature] - mean) / std
+    X_validation.loc[:, feature] = (X_validation[feature] - mean) / std
+for feature in X_train_.columns.values[:-1]:
+    mean, std = data_resampled[feature].mean(), data_resampled[feature].std()
+    X_train_.loc[:, feature] = (X_train_[feature] - mean) / std
+    X_test_.loc[:, feature] = (X_test_[feature] - mean) / std
+    X_validation_.loc[:, feature] = (X_validation_[feature] - mean) / std
+
+
+# Create labels
+y_train = X_train.Class
+y_test = X_test.Class
+y_validation = X_validation.Class
+y_train_ = X_train_.Class
+y_test_ = X_test_.Class
+y_validation_ = X_validation_.Class
+# Remove labels from X's
+X_train = X_train.drop(['Class'], axis=1)
+X_train_ = X_train_.drop(['Class'], axis=1)
+X_test = X_test.drop(['Class'], axis=1)
+X_test_ = X_test_.drop(['Class'], axis=1)
+X_validation = X_validation.drop(['Class'], axis=1)
+X_validation_ = X_validation_.drop(['Class'], axis=1)
+
+# Pickle and save the dataset
+dataset = {'X_train' : X_train,
+           'X_train_': X_train_,
+           'X_test': X_test,
+           'X_test_': X_test,
+           'X_validation': X_validation,
+           'X_validation_': X_validation_,
+           'y_train': y_train,
+           'y_train_': y_train_,
+           'y_test': y_test,
+           'y_test_': y_test_,
+           'y_validation': y_validation,
+           'y_validation_': y_validation_}
+#with open('pickle/data_with_resample_apr19.pkl', 'wb+') as f:
+ #   pickle.dump(dataset, f)
+
+# Random Forest on unsampled training data
+# rf_n_est_100_unsampled_unweighted
+rf = RandomForestClassifier(n_estimators=100, oob_score=True, n_jobs=4)
+rf.fit(dataset['X_train'], dataset['y_train'])
+y_pred = rf.predict(dataset['X_test'])
+plot_confusion_matrix(dataset['y_test'], y_pred)
